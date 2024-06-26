442424158963fc778ba8bd1a7d6a1306847ad8e3
Banasri Gupta <banasrigupta123@gmail.com>
2024-06-26 12:45:08 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    red: 0,
+    green: 0,
+    blue: 0
+  });
+
+
+  const [result, setResult] = useState("#FFFFFF");
+  const [error, setError] = useState("");
+
+  function decimalToHex(decimal) {
+    return decimal.toString(16).padStart(2, '0');
+  }
+  function handleChange(e) {
+    const { name, value } = e.target;
+    const newValue = parseInt(value);
+    setFormData((prevData) => {
+      const newFormData = { ...prevData, [name]: newValue };
+      const newColor = "#" +
+        decimalToHex(newFormData.red) +
+        decimalToHex(newFormData.green) +
+        decimalToHex(newFormData.blue);
+
+      if (isNaN(newValue) || newValue < 0 || newValue > 255) {
+        setResult("#FFFFFF");
+        setError("Error");
+      } else {
+        const newColor = "#" + decimalToHex(newFormData.red) + decimalToHex(newFormData.green) + decimalToHex(newFormData.blue);
+        setError("");
+        setResult(newColor);
+      }
+
+      return newFormData;
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Color Code Generator</p>
+      <form>
+        <label>Red</label>
+        <input type="number" name="red" value={formData.red} onChange={handleChange}></input>
+        <label>Green</label>
+        <input type="number" name="green" value={formData.green} onChange={handleChange}></input>
+        <label>Blue</label>
+        <input type="number" name="blue" value={formData.blue} onChange={handleChange}></input>
+      </form>
+      <p className='box' style={{ backgroundColor: result }}>{error}</p>
     </div>
   );
 }
